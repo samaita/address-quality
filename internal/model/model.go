55b71d5b51941e31242357d27cdad02078ba4402
Samaita <garysamaita@gmail.com>
2026-07-11 20:28:56 +0700

    extract address validation into model.AddressRequest.Validate()

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,7 +1,22 @@
 package model
 
+import (
+	"errors"
+	"fmt"
+)
+
 type AddressRequest struct {
 	Address string `json:"address" validate:"required"`
+}
+
+func (r *AddressRequest) Validate(maxLength int) error {
+	if r.Address == "" {
+		return errors.New("address is required")
+	}
+	if len(r.Address) > maxLength {
+		return fmt.Errorf("address exceeds maximum length of %d characters", maxLength)
+	}
+	return nil
 }
 
 type Location struct {
