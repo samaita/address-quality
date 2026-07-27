335e04ea2c088ae900aa4edfe1adfb05652adaeb
Samaita <garysamaita@gmail.com>
2026-07-27 22:35:33 +0700

    feat: add postal code prefix match scoring with scorePostalCodePrefix helper

--- internal/service/constant.go
+++ internal/service/constant.go
@@ -14,6 +14,9 @@ const (
 	WeightMultiProvince    = 0.20
 	WeightMultiCity        = 0.15
 	WeightMultiDistrict    = 0.10
-	WeightMultiSubDistrict = 0.05
-	MaxMultiEvidenceBonus  = 0.40
+	WeightMultiSubDistrict       = 0.05
+	MaxMultiEvidenceBonus        = 0.40
+	WeightPostalCodePrefix3      = 0.07
+	WeightPostalCodePrefix4      = 0.10
+	WeightPostalCodePrefix5      = 0.20
 )
